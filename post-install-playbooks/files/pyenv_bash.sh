eca19c269ce03c3672beff07de0b8bc37741fd48
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-12 12:05:16 -0400

    yet more evals

--- post-install-playbooks/files/pyenv_bash.sh
+++ post-install-playbooks/files/pyenv_bash.sh
@@ -3,4 +3,5 @@
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
